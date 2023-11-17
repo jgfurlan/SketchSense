c5d92a254838f4398a61acd74aad79d494cef402
jgfurlan <joaogfurlan@hotmail.co.uk>
2023-11-17 20:52:01 +0000

    tnc

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -1,27 +1,40 @@
 class SketchPad {
-constructor(container, size = 400) {
+  constructor(container, size = 400) {
     this.canvas = document.createElement("canvas");
     this.canvas.width = size;
     this.canvas.height = size;
-    this.canvas.style =`
+    this.canvas.style = `
       background-color: white;
       box-shadow: 0px 0px 10px 2px black;
     `;
     container.appendChild(this.canvas);
-  
+
     this.ctx = this.canvas.getContext("2d");
 
-    this.#addEventListeners();
-}
+    this.path = [];
+    this.isDrawing = false;
 
-#addEventListeners(){
-    this.canvas.onmousedown=(evt)=>{
-        const rect=this.canvas.getBoundingClientRect();
-        const mouse=[
-            Math.round(evt.clientX-rect.left),
-            Math.round(evt.clientY-rect.top)
-        ];
-        console.log(mouse);
-    }
+    this.#addEventListeners();
+  }
+
+  #addEventListeners() {
+    this.canvas.onmousedown = (evt) => {
+      const rect = this.canvas.getBoundingClientRect();
+      const mouse = [
+        Math.round(evt.clientX - rect.left),
+        Math.round(evt.clientY - rect.top),
+      ];
+      this.path = [mouse];
+      this.isDrawing = true;
+    };
+    this.canvas.onmousedown = (evt) => {
+      const rect = this.canvas.getBoundingClientRect();
+      const mouse = [
+        Math.round(evt.clientX - rect.left),
+        Math.round(evt.clientY - rect.top),
+      ];
+      this.path = [mouse];
+      this.isDrawing = true;
+    };
   }
 }
